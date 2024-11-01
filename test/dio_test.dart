c0a3edd9ae382ac1bda977109d3617c2f2963d49
red-app-stack <redapp.stack@mail.ru>
2024-11-01 21:16:51 +0500

    tests

--- test/dio_test.dart
+++ test/dio_test.dart
@@ -3,14 +3,17 @@ import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-void main() {
+void main() async {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
   test('DIO connection test', () async {
+    print('Api in test: ${dotenv.env['API_URL_1'] ?? ''}');
+
     final dio = Dio(BaseOptions(
       baseUrl: dotenv.env['API_URL_1'] ?? '',
       connectTimeout: Duration(seconds: 10),
       receiveTimeout: Duration(seconds: 10),
     ));
-    dio.interceptors.add(AuthInterceptor());
     try {
       final response = await dio.get('/');
       expect(response.statusCode, 404);
